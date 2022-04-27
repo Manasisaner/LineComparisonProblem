5c6c25e78341df201181e32f4ff4febf5e3facf3
snehasonwane <snehasonwane01@gmail.com>
2022-04-28 00:17:46 +0530

    check greater lessthan line

--- Linecomparison/Linecomparison/Program.cs
+++ Linecomparison/Linecomparison/Program.cs
@@ -6,7 +6,28 @@ length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 Console.WriteLine("length of the line = " + length);*/
 
 
+/*int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
+int a = 0;
+int b = 0;
+Console.WriteLine(a == b);*/
+
 int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
 int a = 0;
 int b = 0;
 Console.WriteLine(a == b);
+// compare both the values
+// using CompareTo() method
+int status = a.CompareTo(b);
+
+// checking the status
+if (status > 0)
+    Console.WriteLine("{0} is greater than {1}",
+                                a, b);
+else if (status < 0)
+    Console.WriteLine("{0} is less than {1}",
+                                a, b);
+else
+    Console.WriteLine("{0} is equal to {1}",
+                                a, b);
+
+
