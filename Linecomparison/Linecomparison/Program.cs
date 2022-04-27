15387680f4097b27f7b764948faf80607e8dd6d8
snehasonwane <snehasonwane01@gmail.com>
2022-04-28 00:09:26 +0530

    check equal length

--- Linecomparison/Linecomparison/Program.cs
+++ Linecomparison/Linecomparison/Program.cs
@@ -1,6 +1,12 @@
 ﻿
-Console.WriteLine("Welcome to Line Comparison Computation Program");
+/*Console.WriteLine("Welcome to Line Comparison Computation Program");
 int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
 double length = 0;
 length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-Console.WriteLine("length of the line = " + length);
+Console.WriteLine("length of the line = " + length);*/
+
+
+int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
+int a = 0;
+int b = 0;
+Console.WriteLine(a == b);
